b8848711a4321947fdffc683db95d325a4741763
Andres pinzon <ampinzonv@unal.edu.co>
2021-12-15 15:53:52 +0000

    Stripped bashrc file

--- check_dependencies.sh
+++ check_dependencies.sh
@@ -3,7 +3,7 @@
 source lib/bash-utility/src/check.sh
 source lib/shml/shml.sh
 
-soft=('Ab' 'CD' 'embossversion' 'gcc' 'zless' 'wget' 'curl' 'unzip' 'gnuplot')
+soft=('Ab' 'blastp' 'embossversion' 'gcc' 'zcat' 'wget' 'curl' 'unzip' 'gnuplot')
 
 echo "Checking external dependencies:"
 
@@ -11,13 +11,15 @@ for i in "${soft[@]}"; do
   
   check::command_exists "$i"
 
+  # Capture function return value. Can be 0 (found),1 (not found) or 
+  # 2 (missing arguments)
   status=$?
 
   if (( $status > 0 ));then
-  	echo "$i command not found"
+  	echo "$i seems to be not installed and it is required by BioBash."
   fi
  
   #TODO populate not found software array
-  #if array is not empty stop installation.
+  #if array is not empty stop installation or suggest instalaltion using APT or so.
 done
 
