76a226b5b466dda03bd040d29c243c74fadcca84
ANDRES M PINZON V <andres@ANDRESs-MacBook-Air.local>
2022-05-03 19:48:34 -0500

    Improved check requirements for installation.

--- setup_environment.sh
+++ setup_environment.sh
@@ -8,16 +8,36 @@ Setting up environment. This will modify your $HOME/.bashrc file.
 
 "
 
-#Check that $HOME var is not empty
-#todo
+#Check that $HOME var is not empty. "-z" means IT IS EMPTY.
+if [[ -z "${HOME}" ]];then
+	echo "The HOME variable is empty (that's weird!). Please fix this issue before installing BioBash."
+	exit 1
+fi
 
-#check that  .bashrc file exists
-#todo
+#Check that $HOME var is writable (one never knows).
+if [ ! -w "${HOME}" ];then
+	echo "I can not write to HOME: ${HOME}. Please make sure you have the right permissions."
+	exit 1
 
-#backup .bashrc file
-#todo
+fi
+
+# If we reached this point HOME is a valid path and it is writable.
+#check that  ~/.bashrc file exists
+if [[ ! -e "${HOME}/.bashrc" ]];then
+	echo "bashrc file is not present, so I will create one for you."
+	touch ${HOME}/.bashrc
+fi
+
+# If bashrc file is present, let's backup the actual .bashrc file
+echo "A 'bashrc' file was found at $HOME. I will make a backup of your actual file with
+the extension '_BB.backup'."
+
+rightNow=$(date +%m-%d-%Y)
+cp $HOME/.bashrc $HOME/.bashrc_BB.backup.${rightNow}
 
 
+#This $1 variable comes from installbiobash script. Basically it is the installDir path +
+# biobash version, converted into a directory.
 BIOBASH_HOME=$1
 
 BIOBASH_LIB="$BIOBASH_HOME/lib"
@@ -29,13 +49,13 @@ BASHUTILITY_LIB="$BASHUTILITY_LIB_PATH/bash_utility.sh"
 BB_NATIVE_LIB_PATH="$BIOBASH_LIB/bb_native"
 BB_NATIVE_LIB="$BB_NATIVE_LIB_PATH/bb_native.sh"
 
-rightnow=$(date +%m-%d-%Y)
+
 
 echo "
-#
+#                          - BIOBASH -
 # This section was automatically added to your .bashrc file by the
-# BIOBASH installer on: ${rightnow}.
-# A copy of your original .bashrc file was created with name: .bashrc_${rightnow}
+# BIOBASH installer on: ${rightNow}.
+# A copy of your original .bashrc file was created with name: .bashrc_BB.backup.${rightNow}
 
 # DO NOT modify below unless you are completely sure what you are doing
 # otherwise BIOBASH may not function at all.
@@ -61,3 +81,7 @@ export BB_NATIVE_LIB_PATH
 export BB_NATIVE_LIB
 
 " >> $HOME/.bashrc
+
+#Make all variables accesible to this shell.
+source $HOME/.bashrc
+exit 0
